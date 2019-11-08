f113b861298f655d218ccaf5f05510185512640a
Lam Nguyen <lam.nguyen.hy.hoai@gmail.com>
2019-11-08 09:50:12 +0700

    Fixed typos

--- hooks/ow_hook.test.c
+++ hooks/ow_hook.test.c
@@ -9,14 +9,14 @@ int main(int argc, char *argv[])
 {
     char file_name[] = "test.txt";
 
-    // Create a file readable/writable.
+    // Create a readable/writable file.
     syscall(SYS_open, file_name, O_CREAT, S_IRUSR | S_IWUSR);
 
-    // Open as write only
+    // Open as write-only
     int fd = syscall(SYS_open, file_name, O_WRONLY);
 
     // Write something
-    syscall(SYS_write, fd, "hfllo", 5);
+    syscall(SYS_write, fd, "hello", 5);
 
     // Close the file
     syscall(SYS_close, fd);
